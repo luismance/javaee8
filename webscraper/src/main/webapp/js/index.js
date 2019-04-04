bd77b0587e3b48a52d85d25d90e37f7ed36aff8e
Luis Mance <luis.mance@manaty.net>
2019-04-04 23:53:53 +0800

    updated ui using reactjs

--- webscraper/src/main/webapp/js/index.js
+++ webscraper/src/main/webapp/js/index.js
@@ -5,38 +5,86 @@ class SellerList extends React.Component {
 	    this.state = {
 	    sellerurl : '',
 	      sellername : '',
-	      items : []
+	      items : [],
+				error : '',
+				errorInfo : '',
+				pageNum : 1,
+				pageSize : 10
 	    };
-	    
-	    this.componentDidMount = this.componentDidMount.bind(this);
 	  }
-	
-	componentDidMount(){
+
+	updateItems = (pageNum, pageSize) => {
 		var thisComp = this;
-		if(thisComp.state.sellerurl){
+		var sellerUrl = $('#websiteurl').val();
+		console.log("Updating Items from "+sellerUrl);
+		if(sellerUrl){
 		$.ajax({
 		      type: "GET",
-		      url: "rest/rest/parse/tipidpc?url=" + this.state.sellerurl,
+		      url: "rest/parse/tipidpc?url=" + sellerUrl+"&pageNum="+pageNum+"&pageSize="+pageSize,
 		      headers: {
 		        "Content-Type": "application/json"
 		      },
 		      dataType: "text",
 		      success: function(data) {
-		        var sellerDetails = data.itemsForSale;
-		        thisComp.setState({ items : sellerDetails });
+						var dataObj = JSON.parse(data);
+						var sellerId = dataObj.id;
+		        var sellerDetails = dataObj.itemsForSale;
+		        thisComp.setState({
+							sellername : dataObj.id,
+							pageNum,
+							pageSize,
+							items : sellerDetails });
 		      },
 		      error: function(data) {
 		        console.log("Error : " + JSON.stringify(data));
 		      }
 		    });
+			}
 	}
-	}
-	
+
+	componentDidCatch(error, errorInfo) {
+    this.setState({
+      error: error,
+			errorInfo: errorInfo,
+    })
+  }
+
 	render(){
+		var itemList = this.state.items.map((item, i) => (
+      <tr key={item.id}>
+				<td>
+					{item.name}
+				</td>
+				<td>
+					{item.price}
+				</td>
+				<td>
+					{
+						item.imgUrls.map((img, i) => (
+							<img key={i} src={img} height="42" width="42" />
+						))
+					}
+				</td>
+			</tr>
+    ));
+
 		return(
-		
+
 				<div>
-					<label>Input URL : </label><input id="websiteurl" type="text" />
+					<h3>{this.state.errorInfo ? this.state.errorInfo : ""}</h3>
+					<label>Input URL : </label>
+					<input id="websiteurl" type="text" />
+					<button id="btnUpdateItem"  onClick={() => this.updateItems(1,10)}>Update List</button>
+					<h3>{this.state.sellername}</h3>
+					<h4>page : {this.state.pageNum}</h4>
+					<table width="100%">
+					<tbody>
+					{itemList}
+					</tbody>
+					</table>
+					<a style={{ visibility : (this.state.pageNum > 1 ? "visible" : "hidden") }} onClick={() => this.updateItems(this.state.pageNum-1,this.state.pageSize)} >{"<"}</a>
+					<a onClick={() => this.updateItems(this.state.pageNum+1,this.state.pageSize)}>></a>
+
 				</div>
 		)
 	}
